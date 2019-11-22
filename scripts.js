7c04a56a2540766a84a6eae6081413cfd504bd14
Abhishek Samariya <asamariya@gmail.com>
2019-11-22 18:41:35 +1100

    Add js so that we toggle 'selected' class when we go to next page, getting the info from barba's beforeEnter hook

--- scripts.js
+++ scripts.js
@@ -3,7 +3,17 @@ barba.init({
   transitions: [
     {
       name: 'fade',
-      beforeEnter() {
+      beforeEnter({ current, next, trigger }) {
+        const headerLinks = document.querySelectorAll('header a');
+        const href = next.url.path;
+        headerLinks.forEach(link => {
+          if (link.getAttribute('href') === href) {
+            link.classList.add('selected');
+          } else {
+            link.classList.remove('selected');
+          }
+        });
+
         window.scrollTo({
           top: 0,
           behavior: 'smooth'
